5b8a7eb2f941d6dfd8b7ea0978b8b876e6697615
KiteHolster <park.hw@hotmail.com>
2023-08-10 19:34:24 +1000

    Initial Commit to add the project template to the repository

---
+++ FIT5032_major_assignment/FIT5032_major_assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_major_assignment.MvcApplication" Language="C#" %>
---
+++ FIT5032_major_assignment/FIT5032_major_assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_major_assignment.Startup))]
+namespace FIT5032_major_assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
