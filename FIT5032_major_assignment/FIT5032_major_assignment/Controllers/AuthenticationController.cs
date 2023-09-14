43ca9ab954f20050d302d3d2f2d8a70a4688f11d
KiteHolster <park.hw@hotmail.com>
2023-09-14 16:12:00 +1000

    updates

--- FIT5032_major_assignment/FIT5032_major_assignment/Controllers/AuthenticationController.cs
+++ FIT5032_major_assignment/FIT5032_major_assignment/Controllers/AuthenticationController.cs
@@ -8,6 +8,10 @@ namespace FIT5032_major_assignment.Controllers
 {
     public class AuthenticationController : Controller
     {
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
         // GET: Authentication
         public ActionResult Index()
         {
